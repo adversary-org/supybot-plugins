4bf31efd9c9862532d74ffade685be4adf95daaf
Ben McGinnes <ben@adversary.org>
2018-01-16 21:06:42 +1100

    Moved local to local
    * Moved cargs.py and the requests call to the local module to import
      to the plugin from there.

--- fiscal/btcmarkets/BTCAUD/local/__init__.py
+++ fiscal/btcmarkets/BTCAUD/local/__init__.py
@@ -1,1 +1,15 @@
 # Stub so local is a module, used for third-party modules
+import requests
+
+from .cargs import permanent
+ca = permanent.client_args
+domain = "https://api.btcmarkets.net"
+uri = "/market/BTC/AUD/tick"
+url = "{0}{1}".format(domain, uri)
+try:
+    r = requests.get(url, verify=True, headers=ca["headers"],
+                     proxies=ca["proxies"])
+    # r = requests.get(url, verify=True)
+except:
+    r = None
+    pass
--- fiscal/btcmarkets/BTCAUD/plugin.py
+++ fiscal/btcmarkets/BTCAUD/plugin.py
@@ -29,21 +29,9 @@
 ###
 
 import datetime
-import requests
 import time
-
-from .cargs import permanent
-ca = permanent.client_args
-domain = "https://api.btcmarkets.net"
-uri = "/market/BTC/AUD/tick"
-url = "{0}{1}".format(domain, uri)
-try:
-    r = requests.get(url, verify=True, headers=ca["headers"],
-                     proxies=ca["proxies"])
-    # r = requests.get(url, verify=True)
-except:
-    r = None
-    pass
+# import requests
+from local import *
 
 import supybot.utils as utils
 from supybot.commands import *
